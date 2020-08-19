9180bd888ec8239496dd4ffee97d0b1510789c48
Kevin <ktb92677@yahoo.com>
2020-08-19 11:32:49 -0700

    kord

--- CURRENCY.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CURRENCY_H_
-#define _CURRENCY_H_
-
-typedef int CURRENCY;
-
-bool Init_CURRENCY(CURRENCY*, const char*);
-
-#endif _CURRENCY_H_
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 /*
 
@@ -6,7 +6,22 @@
 
 */
 
-int main() {
-	printf("Hello, World1234!\n");
-	return 0;
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double base, power, result;
+
+    printf("Enter the base number: ");
+    scanf("%lf", &base);
+
+    printf("Enter the power raised: ");
+    scanf("%lf",&power);
+
+    result = pow(base,power);
+
+    printf("%.1lf^%.1lf = %.2lf", base, power, result);
+
+    return 0;
 }
